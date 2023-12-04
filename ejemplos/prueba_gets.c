e39872907b6118cfb3a5d0f3190be66c788e1785
Almudena Garcia <liberamenso10000@gmail.com>
2023-12-04 21:52:15 +0100

    borrado contenido de c++ de fichero c

---
+++ ejemplos/prueba_gets.c
@@ -0,0 +1,16 @@
+//#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  char cadena2[11] = "cadena2";
+  char cadena1[10] = "prueba";
+  
+  printf("Introduce una palabra: ");
+  gets(cadena1);
+  
+  printf("El contenido de la cadena1 es %s\n", cadena1);
+  printf("El contenido de la cadena2 es %s\n", cadena2);
+  
+  return 0;
+}
